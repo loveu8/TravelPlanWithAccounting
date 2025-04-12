e6cbce67f41383ecb70f48a2816aa2822572caca
Ken-Valentino <freshscare@gmail.com>
2025-04-12 16:54:57 +0800

    測試commit

--- backend/src/main/java/com/travelPlanWithAccounting/service/controller/HelloController.java
+++ backend/src/main/java/com/travelPlanWithAccounting/service/controller/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String hello() {
         return "Hello World";
     }
+
+    @GetMapping("/haaaa")
+    public String haaaa(){
+        return "test git commit";
+    }
 }
