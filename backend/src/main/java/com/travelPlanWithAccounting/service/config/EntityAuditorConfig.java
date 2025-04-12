973b3d9e99e194f43d2c4ef1f3ec25b41fb9659a
drjs07 <drjs07@gmail.com>
2025-04-12 15:39:43 +0800

    chore: 調整程式format

--- backend/src/main/java/com/travelPlanWithAccounting/service/config/EntityAuditorConfig.java
+++ backend/src/main/java/com/travelPlanWithAccounting/service/config/EntityAuditorConfig.java
@@ -1,23 +1,20 @@
 package com.travelPlanWithAccounting.service.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Optional;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
-
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class EntityAuditorConfig implements AuditorAware<String> {
 
-    final HttpServletRequest request;
+  final HttpServletRequest request;
 
-    @Override
-    public Optional<String> getCurrentAuditor() {
-        // TODO: implement logic to get the current auditor
-        return Optional.of("system");
-    }
-
+  @Override
+  public Optional<String> getCurrentAuditor() {
+    // TODO: implement logic to get the current auditor
+    return Optional.of("system");
+  }
 }
