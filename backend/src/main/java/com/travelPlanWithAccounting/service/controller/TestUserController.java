0aa5efa86a6d26639718f5bb39234f7811253433
SL-ASUSROGGM501\StarQk <qkpigstar@gmail.com>
2025-04-12 18:21:53 +1000

    format

--- backend/src/main/java/com/travelPlanWithAccounting/service/controller/TestUserController.java
+++ backend/src/main/java/com/travelPlanWithAccounting/service/controller/TestUserController.java
@@ -1,22 +1,21 @@
 package com.travelPlanWithAccounting.service.controller;
 
+import com.travelPlanWithAccounting.service.entity.TestUser;
+import com.travelPlanWithAccounting.service.service.TestUserService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.travelPlanWithAccounting.service.entity.TestUser;
-import com.travelPlanWithAccounting.service.service.TestUserService;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class TestUserController {
 
-    @Autowired
-    private TestUserService testUserService;
+  @Autowired private TestUserService testUserService;
 
-    @GetMapping
-    public List<TestUser> getAllUsers() {
-        return testUserService.getAllUsers();
-    }
+  @GetMapping
+  public List<TestUser> getAllUsers() {
+    return testUserService.getAllUsers();
+  }
 }
