c87e2ee8d69017cef49ddff15a338e2f47c26d36
shuyi <shuyi041@gmail.com>
2025-05-03 08:47:16 +0800

    feat: add size attr and adjust variant for button component

--- frontend/src/app/components/Button/index.tsx
+++ frontend/src/app/components/Button/index.tsx
@@ -6,16 +6,18 @@ import IButtonProps from "./button.types";
 
 export default function Button({
   text,
+  size = "3",
   isMain = true,
   icon,
   isDisabled = false,
   handleClick = () => {},
 }: IButtonProps) {
   const btnColor = isMain ? "blue" : "gray";
-  const btnVariant = isMain ? "solid" : "outline";
+  const btnVariant = isMain ? "solid" : "surface";
 
   return (
     <RadixButton
+      size={size}
       color={btnColor}
       variant={btnVariant}
       className={isDisabled ? "" : "cursor-pointer"}
