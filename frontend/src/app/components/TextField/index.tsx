d3c141ae7f843ff59e74dbaca27d0ec82395aeb0
Lana Su <lanamaysu@gmail.com>
2025-05-08 22:59:38 +0800

    fix: ensure unique id for TextField

--- frontend/src/app/components/TextField/index.tsx
+++ frontend/src/app/components/TextField/index.tsx
@@ -1,16 +1,20 @@
+import { useId } from "react";
 import { Root, Slot } from "@radix-ui/themes/components/text-field";
 import { Text } from "@radix-ui/themes";
 import ITextFieldProps from "./text-field.types";
 
 function TextField({ size = "3", label, ...props }: ITextFieldProps) {
+  const generatedId = useId();
+  const id = props.id || generatedId;
+
   return (
     <>
       {label && (
         <Text asChild size={size}>
-          <label htmlFor={props.id}>{label}</label>
+          <label htmlFor={id}>{label}</label>
         </Text>
       )}
-      <Root my="1" size={size} {...props} />
+      <Root my="1" size={size} id={id} {...props} />
     </>
   );
 }
