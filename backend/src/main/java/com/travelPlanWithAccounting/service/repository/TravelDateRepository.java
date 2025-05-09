ee5505e76e2c36f566c09675571937a2a9c90a9b
Ken-Valentino <freshscare@gmail.com>
2025-05-09 22:43:26 +0800

    試寫repository+service

---
+++ backend/src/main/java/com/travelPlanWithAccounting/service/repository/TravelDateRepository.java
@@ -0,0 +1,14 @@
+package com.travelPlanWithAccounting.service.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.travelPlanWithAccounting.service.entity.TravelDate;
+
+@Repository
+public interface TravelDateRepository extends JpaRepository<TravelDate, UUID> {
+    List<TravelDate> findByTravelMainId(UUID travelMainId);
+}
