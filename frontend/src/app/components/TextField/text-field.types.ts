303d758ce21a2d66b05d82118de1a0a4fa0b7cb6
Lana Su <lanamaysu@gmail.com>
2025-05-09 21:35:40 +0800

    feat: 補上 size = "1"

--- frontend/src/app/components/TextField/text-field.types.ts
+++ frontend/src/app/components/TextField/text-field.types.ts
@@ -1,7 +1,7 @@
 import { RootProps, SlotProps } from "@radix-ui/themes/components/text-field";
 
 export default interface ITextFieldProps extends RootProps {
-  size?: "2" | "3";
+  size?: "1" | "2" | "3";
   radius?: "small" | "medium" | "large";
   label?: string;
   className?: string;
