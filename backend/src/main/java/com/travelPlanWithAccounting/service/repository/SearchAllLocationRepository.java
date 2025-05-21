770565c6904c542446b99128f307ab4cd397057e
SL-ASUSROGGM501\StarQk <qkpigstar@gmail.com>
2025-05-21 19:46:30 +1000

    新增Alllocation方便搜尋引擎篩選

---
+++ backend/src/main/java/com/travelPlanWithAccounting/service/repository/SearchAllLocationRepository.java
@@ -0,0 +1,19 @@
+package com.travelPlanWithAccounting.service.repository;
+
+import com.travelPlanWithAccounting.service.entity.Location;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SearchAllLocationRepository extends JpaRepository<Location, UUID> {
+
+  @Query(
+      "SELECT l, lm.textContent AS countryName "
+          + "FROM Location l "
+          + "LEFT JOIN l.metadata lm ON lm.locationMetaType = '001' AND l.id = lm.location.id "
+          + "ORDER BY l.code")
+  List<Object[]> findAllLocation();
+}
