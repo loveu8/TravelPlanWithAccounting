b9c56713844ab15b746ec5b0001f1f31137f8bf4
drjs07 <drjs07@gmail.com>
2025-06-22 17:04:20 +0800

    refactor: remove Serializable requirement from response classes and update related methods for improved flexibility

--- backend/src/main/java/com/travelPlanWithAccounting/service/dto/otp/OtpStatusResponse.java
+++ backend/src/main/java/com/travelPlanWithAccounting/service/dto/otp/OtpStatusResponse.java
@@ -1,6 +1,5 @@
 package com.travelPlanWithAccounting.service.dto.otp;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,8 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class OtpStatusResponse implements Serializable {
-  private static final long serialVersionUID = 1L;
+public class OtpStatusResponse {
   private boolean exists;
   private Boolean expired;
   private Integer attemptCount;
