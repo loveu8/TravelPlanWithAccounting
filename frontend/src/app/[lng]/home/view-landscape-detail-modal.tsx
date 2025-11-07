4bf7a431c4f45c081dde74c8ad1c2dc734e7bae3
shuyi <shuyi041@gmail.com>
2025-11-07 23:13:46 +0800

    feat: refactor ViewLandscapeDetailModal to use forwardRef and implement toggle functionality

--- frontend/src/app/[lng]/home/view-landscape-detail-modal.tsx
+++ frontend/src/app/[lng]/home/view-landscape-detail-modal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, forwardRef, useImperativeHandle } from "react";
 import {
   DialogRoot,
   DialogContent,
@@ -13,49 +13,120 @@ import ViewLandscapeDetailCard from "@/app/components/Card/style/view-landscape-
 
 /*
  * ViewLandscapeDetailModal 元件 Props
- * @type {Object} ITravelPlanCardProps
- * @extends ILandscapeDetailCardProps
- * @property {boolean} open - 是否打開彈窗
- * @property {function} onOpenChange - 控制彈窗開關的函數
- * @property {string} title - 彈窗標題
- * @property {string} iframeSrc - 嵌入的iframe的URL
+ * @property {function} handleBookmarkClick - 處理收藏按鈕點擊的函數
+ * @property {function} handleAddScheduleClick - 處理添加行程按鈕點擊的函數
  */
-interface ITravelPlanCardProps extends ILandscapeDetailCardProps {
-  open: boolean;
-  onOpenChange: (open: boolean) => void;
-  iframeSrc: string;
+interface ITravelPlanCardProps {
+  handleBookmarkClick: (id: string) => void;
+  handleAddScheduleClick: (id: string) => void;
 }
 
-export default function ViewLandscapeDetailModal({
-  open,
-  onOpenChange,
-  title,
-  iframeSrc,
-  ...props
-}: ITravelPlanCardProps) {
-  return (
-    <DialogRoot open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="min-w-[80vw] pb-0" headerWithClose>
-        <DialogHeader title={title} />
-        <DialogBody className="relative overflow-hidden px-0">
-          <Grid
-            as="div"
-            columns="420px 1fr"
-            align="start"
-            className="min-h-[75vh]"
-          >
-            <ViewLandscapeDetailCard title={title} {...props} />
-            <iframe
-              width="100%"
-              height="100%"
-              loading="lazy"
-              allowFullScreen
-              referrerPolicy="no-referrer-when-downgrade"
-              src={iframeSrc}
-            />
-          </Grid>
-        </DialogBody>
-      </DialogContent>
-    </DialogRoot>
-  );
+type IModalDataType = Omit<
+  ILandscapeDetailCardProps,
+  "id" | "handleAddScheduleClick" | "handleBookmarkClick"
+> & {
+  iframeSrc: string;
+};
+
+export interface IViewLandscapeDetailImperativeHandle {
+  open: () => void;
+  close: () => void;
+  toggle: (id: string) => void;
 }
+
+const ViewLandscapeDetailModal = forwardRef(
+  (
+    { handleAddScheduleClick, handleBookmarkClick }: ITravelPlanCardProps,
+    ref: React.ForwardedRef<IViewLandscapeDetailImperativeHandle>,
+  ) => {
+    const [open, setOpen] = useState(false);
+    const [iframeSrc, setIframeSrc] = useState<string>("");
+    const [landscapeDetail, setLandscapeDetail] =
+      useState<ILandscapeDetailCardProps | null>(null);
+
+    const getLandscapeDetail = (id: string) => {
+      // TODO: 串取景點詳細資料API
+      console.log("Fetching landscape detail ID:", id);
+      const fakeFetchedData: IModalDataType = {
+        title: "東京迪士尼",
+        iframeSrc: "https://www.google.com/maps?q=東京迪士尼&output=embed",
+        imgSrc: "/img/Japan.jpg",
+        score: 4.5,
+        evaluateCount: 100,
+        isBookmarked: true,
+        details: {
+          description:
+            "著名主題公園的東京分支，以遊樂設施、現場表演和古裝人物聞名",
+          address: "1-1 Maihama, Urayasu, Chiba 279-0031日本",
+          phone: "+81453305211",
+          website: "https://www.tokyodisneyresort.jp/tdl/",
+          hours: [
+            "星期日 08:00–22:00",
+            "星期六 08:00–22:00",
+            "星期六 08:00–22:00",
+            "星期六 08:00–22:00",
+            "星期六 08:00–22:00",
+          ],
+        },
+      };
+
+      const { iframeSrc, ...fakeData } = fakeFetchedData;
+      setLandscapeDetail({
+        ...fakeData,
+        id,
+        handleAddScheduleClick: () => handleAddScheduleClick(id),
+        handleBookmarkClick: handleBookmarkClick,
+      });
+      setIframeSrc(iframeSrc);
+    };
+
+    useImperativeHandle(ref, () => ({
+      open: () => {
+        setOpen(true);
+      },
+      close: () => {
+        setOpen(false);
+      },
+      toggle: (id: string) => {
+        console.log({ id });
+        if (open) {
+          setLandscapeDetail(null);
+        } else {
+          getLandscapeDetail(id);
+        }
+        setOpen((prev) => !prev);
+      },
+    }));
+    return (
+      <DialogRoot open={open} onOpenChange={setOpen}>
+        <DialogContent className="min-w-[80vw] pb-0" headerWithClose>
+          <DialogHeader title={landscapeDetail?.title || ""} />
+          <DialogBody className="relative overflow-hidden px-0">
+            <Grid
+              as="div"
+              columns="420px 1fr"
+              align="start"
+              className="min-h-[75vh]"
+            >
+              {landscapeDetail && (
+                <ViewLandscapeDetailCard {...landscapeDetail} />
+              )}
+              <iframe
+                width="100%"
+                height="100%"
+                loading="lazy"
+                allowFullScreen
+                referrerPolicy="no-referrer-when-downgrade"
+                src={iframeSrc}
+              />
+            </Grid>
+          </DialogBody>
+        </DialogContent>
+      </DialogRoot>
+    );
+  },
+);
+
+ViewLandscapeDetailModal.displayName = "ViewLandscapeDetailModal";
+
+export default ViewLandscapeDetailModal;
