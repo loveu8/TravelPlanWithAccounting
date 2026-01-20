bfa69fcf83200dd22765783d42afb633f8b98564
Shuyi <shuyi041@gmail.com>
2026-01-20 22:12:50 +0800

    feat: enhance TextArea component with error message support

--- frontend/src/app/components/TextArea/index.tsx
+++ frontend/src/app/components/TextArea/index.tsx
@@ -1,21 +1,39 @@
 import { useId } from "react";
 import { TextArea as RadixTextArea } from "@radix-ui/themes/components/text-area";
 import { Text } from "@radix-ui/themes";
+import { cn } from "@/app/lib/utils";
 import ITextAreaProps from "./text-area.types";
 
-function TextArea({ size = "3", label, ...props }: ITextAreaProps) {
+function TextArea({
+  size = "3",
+  label,
+  className,
+  errMsg,
+  ...props
+}: ITextAreaProps) {
   const generatedId = useId();
   const id = props.id || generatedId;
 
   return (
-    <>
+    <div>
       {label && (
         <Text asChild size={size}>
           <label htmlFor={id}>{label}</label>
         </Text>
       )}
-      <RadixTextArea my="1" size={size} id={id} {...props} />
-    </>
+      <RadixTextArea
+        my="1"
+        size={size}
+        id={id}
+        className={cn(className, errMsg && "ring ring-red-500")}
+        {...props}
+      />
+      {errMsg && (
+        <Text size="2" color="red" as="p">
+          {errMsg}
+        </Text>
+      )}
+    </div>
   );
 }
 
--- frontend/src/app/components/TextArea/text-area.types.ts
+++ frontend/src/app/components/TextArea/text-area.types.ts
@@ -3,4 +3,5 @@ import { TextAreaProps } from "@radix-ui/themes/components/text-area";
 export default interface ITextAreaProps extends TextAreaProps {
   label?: string;
   className?: string;
+  errMsg?: string;
 }
